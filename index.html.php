f0b35410413b10c5bd7352cff55b1244f0c2361e
SaurabhAnanta <55853677+Saurabhananta-123@users.noreply.github.com>
2020-05-09 16:27:37 +0530

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,492 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/fa456a1dad.js" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+
+    <section id="title">
+      <div class="container-fluid">
+
+
+      <!-- Nav Bar -->
+
+
+      <body>
+
+        <header>
+
+
+          <nav class="navbar   navbar-expand-lg   navbar-dark  ">
+
+
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03" >
+
+
+               <a class="navbar-brand" href="#">Lawyerhub</a>
+
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+
+              <a class="nav-link" href="#footer">Contact</a>
+          </li>
+
+          <li class="nav-item">
+
+              <a class="nav-link" href="#">Services</a>
+          </li>
+
+          <li class="nav-item">
+
+              <a class="nav-link" href="#">Our people</a>
+          </li>
+          <li class="nav-item">
+
+              <a class="nav-link" href="#">Lawyers</a>
+          </li>
+
+
+      </ul>
+              </div>
+
+          </nav>
+
+    </header>
+
+      </body>
+
+    <h1 class="mhi">VANCOUVER BUSINESS LAWYER.</h1>
+    <h1 class="mhj">NOTHING FANCY </h1> <!-- Title -->
+<hr>
+
+    </section>
+
+
+
+
+<section  class="my-1">
+  <div class="py-3">
+    <h3 class="mytext">Know About Lawyerhub</h3>
+  </div>
+
+
+<div class="container-fluid">
+
+
+          <div class="row">
+
+                  <div class="col-lg-6  col-md-6  col-12">
+                     <img src="Images/A.jpg" class="img-fluid  aboutimage">
+                  </div>
+
+
+                  <div class="col-lg-6  col-md-6  col-12">
+                    <h3 class="display-4">XYZ</h3>
+                  <p class="py-3">A law firm is a business entity formed by one or more lawyers to engage in the practice of law. The primary service rendered by a law firm is to advise clients (individuals or corporations) about their legal rights and responsibilities, and to represent clients in civil or criminal cases,
+                    business transactions.</p>
+
+                    <a href="About.php"  class="btn btn-succes">check more</a>
+                  </div>
+        </div>
+</div>
+
+</section>
+
+
+<section class="my-1">
+  <div class="py-3">
+    <h2 class="mytext">What we offer</h2>
+  </div>
+
+  <div class="container-fluid">
+  <div class="row">
+          <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+          <h4 class="card-title">John Doe</h4>
+          <p class="card-text">Some example text.</p>
+          <hr>
+          <a href="#" class="btn">See Profile</a>
+           </div>
+             </div>
+           </div>
+
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn">See Profile</a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn">See Profile</a>
+          </div>
+        </div>
+      </div>
+
+</div>
+
+<div class="row  py-3" >
+
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn"">See Profile</a>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+
+  <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn">See Profile</a>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" style="width:400px">
+          <img class="card-img-top" src=images/b.jpg alt="Card image">
+          <div class="card-img-overlay">
+            <h4 class="card-title">John Doe</h4>
+            <p class="card-text">Some example text.</p>
+            <a href="#" class="btn">See Profile</a>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+<section id="features class="my-1"">
+
+  <div class="py-3">
+    <h2 class="mytext">Oue Services</h2>
+  </div>
+
+
+    <div class="container-fluid">
+      <div class="row">
+
+
+
+    <div class="col-lg-3 col-md-3 col-12">
+   <i class="fas fa-map-marker-alt fa-customize fa-3x"></i>
+      <h3>Easy to use.</h3>
+      <p>Voted as the Top Vancouver Family Law Firm in Vancouver in 2014 by Top Choice Awards.</p>
+    </div>
+
+
+          <div class="col-lg-3 col-md-3 col-12 ">
+          <i class="fas fa-suitcase fa-customize fa-3x"></i>
+
+            <h3>Elite Clientele.</h3>
+            <p>Voted as the Top Vancouver Family Law Firm in Vancouver in 2014 by Top Choice Awards..</p>
+          </div>
+
+
+                <div class=" col-lg-3 col-md-3 col-12">
+                    <i class="fas fa-cog fa-customize fa-3x"></i>
+                      <h3>Guaranteed to work.</h3>
+                      <p>Voted as the Top Vancouver Family Law Firm in Vancouver in 2014 by Top Choice Awards..</p>
+
+                </div>
+
+
+                <div class=" col-lg-3 col-md-3 col-12">
+                    <i class="far fa-lightbulb fa-customize fa-3x"></i>
+                      <h3>Guaranteed to work.</h3>
+                      <p>Voted as the Top Vancouver Family Law Firm in Vancouver in 2014 by Top Choice Awards..</p>
+
+                </div>
+
+
+
+</div>
+</div>
+</section>
+
+
+<section id="title" class="my-1">
+
+  <div class="py-3">
+    <h2 class="mytext">Who we are</h2>
+  </div>
+
+
+  <div class="container-fluid">
+
+
+
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+      <div class="card text-center">
+              <h4 class="card-title hmargin">John Doe</h4>
+              <p class="card-text">A law firm is a business entity formed by one or more lawyers to engage in the practice of law. The primary service rendered by a law firm is to advise clients (individuals or corporations) about their legal rights and responsibilities, and to represent clients in civil or criminal cases, business transactions.</p>
+              <a href="#" class="btn btn-primary mybtn">Learn more</a>
+            </div>
+          </div>
+
+
+               <div class="col-lg-4 col-md-4 col-12">
+              <div class="card text-center">
+                     <h4 class="card-title hmargin">John Doe</h4>
+                     <p class="card-text">A law firm is a business entity formed by one or more lawyers to engage in the practice of law. The primary service rendered by a law firm is to advise clients (individuals or corporations) about their legal rights and responsibilities, and to represent clients in civil or criminal cases, business transactions..</p>
+                     <a href="#" class="btn btn-primary mybtn">Learn more</a>
+                   </div>
+                 </div>
+
+
+
+
+          <div class="col-lg-4 col-md-4 col-12">
+        <div class="card text-center">
+                <h4 class="card-title hmargin">John Doe</h4>
+                <p class="card-text">A law firm is a business entity formed by one or more lawyers to engage in the practice of law. The primary service rendered by a law firm is to advise clients (individuals or corporations) about their legal rights and responsibilities, and to represent clients in civil or criminal cases, business transactions.</p>
+                <a href="#" class="btn btn-primary mybtn">Learn more</a>
+              </div>
+            </div>
+          </div>
+
+    </div>
+
+    <hr class="mr"></hr>
+
+</section>
+
+
+<section id="features class="my-1"">
+
+  <div class="py-3">
+    <h2 class="mytext">Legal Advice</h2>
+  </div>
+
+
+    <div class="container-fluid">
+      <div class="row">
+
+
+
+    <div class="col-lg-4 col-md-4 col-12">
+
+      <h3>BUSINESS LAW CONSULTATIONS</h3>
+      <p>I work with organizations on privacy law matters, including regulatory compliance and data breach response. These services can be either proactive or reactive. My advice follows PIPEDA, PIPA, GDPR, and anti-spam regulations (CASL) requirements.</p>
+    </div>
+
+
+          <div class="col-lg-4 col-md-4 col-12 ">
+
+
+            <h3>COMMERCIAL AGREEMENTS</h3>
+            <p>I work primarily with service-based enterprises such as engineers, software developers, consultants, marketing agencies, and health professionals. My work focuses on negotiating and drafting services agreements, website policies, and license agreements.</p>
+          </div>
+
+
+                <div class=" col-lg-4 col-md-4 col-12">
+
+                      <h3>INFORMATION SECURITY LAW</h3>
+                      <p>I offer fixed-fee legal consultations. Starting a business? Need independent legal advice? Want your draft agreements reviewed? I can help. These consultations generally take 1 to 2 hours and can be booked in-person, by phone, or by video conference.
+
+Cost: $300</p>
+
+                </div>
+
+
+
+
+
+
+</div>
+</div>
+
+<hr>
+</section>
+
+
+
+
+
+
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+ <!-- Footer Links -->
+ <div class="container text-center text-md-left">
+
+   <!-- Grid row -->
+   <div class="row">
+
+     <!-- Grid column -->
+     <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+       <!-- Content -->
+       <h5 class="font-weight-bold text-uppercase mb-4">Footer Content</h5>
+       <p>Here you can use rows and columns to organize your footer content.</p>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+         esse
+         quasi, veritatis totam voluptas nostrum.</p>
+
+     </div>
+     <!-- Grid column -->
+
+     <hr class="clearfix w-100 d-md-none">
+
+     <!-- Grid column -->
+     <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+       <!-- Links -->
+       <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
+
+       <ul class="list-unstyled">
+         <li>
+           <p>
+             <a href="#!">PROJECTS</a>
+           </p>
+         </li>
+         <li>
+           <p>
+             <a href="#!">ABOUT US</a>
+           </p>
+         </li>
+         <li>
+           <p>
+             <a href="#!">BLOG</a>
+           </p>
+         </li>
+         <li>
+           <p>
+             <a href="#!">AWARDS</a>
+           </p>
+         </li>
+       </ul>
+
+     </div>
+     <!-- Grid column -->
+
+     <hr class="clearfix w-100 d-md-none">
+
+     <!-- Grid column -->
+     <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+       <!-- Contact details -->
+       <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
+
+       <ul class="list-unstyled">
+         <li>
+           <p>
+             <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+         </li>
+         <li>
+           <p>
+             <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+         </li>
+         <li>
+           <p>
+             <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+         </li>
+         <li>
+           <p>
+             <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+         </li>
+       </ul>
+
+     </div>
+     <!-- Grid column -->
+
+     <hr class="clearfix w-100 d-md-none">
+
+     <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+       <!-- Contact details -->
+       <h5 class="font-weight-bold text-uppercase mb-4">Follow US</h5>
+
+       <ul class="list-unstyled">
+         <li>
+           <a type="button" class="btn-floating btn-fb">
+             <i class="fab fa-facebook-f social-icon"></i>
+           </a>
+         </li>
+         <li>
+           <a type="button" class="btn-floating btn-tw">
+             <i class="fab fa-twitter  social-icon"></i>
+           </a>
+         </li>
+         <li>
+           <a type="button" class="btn-floating btn-gplus">
+             <i class="fab fa-google-plus-g social-icon"></i>
+           </a>
+         </li>
+        
+       </ul>
+
+     </div>
+     <!-- Grid column -->
+
+   </div>
+   <!-- Grid row -->
+
+ </div>
+ <!-- Footer Links -->
+
+ <!-- Copyright -->
+ <div class="footer-copyright text-center py-3">© 2020 Copyright:
+   <a href=""> xyz</a>
+ </div>
+ <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
